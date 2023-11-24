d3676afc3ce021a5a9147f681f66fb9b3ed8def1
gv <>
2023-11-25 01:22:03 +0400

    add: delif command

--- src/main.c
+++ src/main.c
@@ -82,51 +82,6 @@ static int bridge_add(const char *name) {
   return 0;
 }
 
-static int bridge_add_iface(const char *br_name, const char *if_name) {
-  int fd = socket(AF_NETLINK, SOCK_RAW, NETLINK_ROUTE);
-  if (fd < 0) {
-    perror("Cannot open netlink socket");
-    return -1;
-  }
-
-  struct sockaddr_nl sa = {
-      .nl_family = AF_NETLINK,
-  };
-
-  struct nlattr attr_master = {.nla_len = 8, .nla_type = IFLA_MASTER};
-  uint8_t buff_nla[512] = {};
-  memcpy(buff_nla, &attr_master, sizeof(attr_master));
-  uint32_t br_index = if_nametoindex(br_name);
-  memcpy(buff_nla + NLA_HDRLEN, &br_index, 4);
-  uint32_t nla_len = NLA_HDRLEN + 4;
-
-  uint32_t message_len =
-      nla_len + sizeof(struct nlmsghdr) + sizeof(struct ifinfomsg);
-
-  uint8_t buff_iov[512] = {};
-  memcpy(buff_iov,
-         &(struct ndm_hdr){.nl_hdr.nlmsg_len = message_len,
-                           .nl_hdr.nlmsg_type = RTM_NEWLINK,
-                           .nl_hdr.nlmsg_flags = NLM_F_REQUEST,
-                           .nl_hdr.nlmsg_seq = (uint32_t)time(NULL),
-                           .info_hdr.ifi_family = AF_UNSPEC,
-                           .info_hdr.ifi_index = if_nametoindex(if_name)},
-         sizeof(struct ndm_hdr));
-  memcpy(buff_iov + sizeof(struct ndm_hdr), buff_nla, nla_len); // TODO size
-  struct iovec iov = {.iov_base = buff_iov, .iov_len = message_len};
-  struct msghdr mh = {
-      .msg_name = &sa,
-      .msg_namelen = sizeof(sa),
-      .msg_iov = &iov,
-      .msg_iovlen = 1,
-  };
-  sendmsg(fd, &mh, 0);
-
-  close(fd);
-
-  return 0;
-}
-
 static int bridge_del(const char *name) {
   int fd = socket(AF_NETLINK, SOCK_RAW, NETLINK_ROUTE);
   if (fd < 0) {
@@ -175,7 +130,57 @@ static int bridge_del(const char *name) {
   return 0;
 }
 
-static int bridge_del_iface(const char *br_name, const char *if_name) {
+static int bridge_mod_iface(unsigned br_index, unsigned if_index);
+
+static int bridge_add_iface(const char *br_name, const char *if_name) {
+  return bridge_mod_iface(if_nametoindex(br_name), if_nametoindex(if_name));
+}
+
+static int bridge_del_iface(const char *if_name) {
+  return bridge_mod_iface(0, if_nametoindex(if_name));
+};
+
+static int bridge_mod_iface(unsigned br_index, unsigned if_index) {
+  int fd = socket(AF_NETLINK, SOCK_RAW, NETLINK_ROUTE);
+  if (fd < 0) {
+    perror("Cannot open netlink socket");
+    return -1;
+  }
+
+  struct sockaddr_nl sa = {
+      .nl_family = AF_NETLINK,
+  };
+
+  struct nlattr attr_master = {.nla_len = 8, .nla_type = IFLA_MASTER};
+  uint8_t buff_nla[512] = {};
+  memcpy(buff_nla, &attr_master, sizeof(attr_master));
+  memcpy(buff_nla + NLA_HDRLEN, &br_index, 4);
+  uint32_t nla_len = NLA_HDRLEN + 4;
+
+  uint32_t message_len =
+      nla_len + sizeof(struct nlmsghdr) + sizeof(struct ifinfomsg);
+
+  uint8_t buff_iov[512] = {};
+  memcpy(buff_iov,
+         &(struct ndm_hdr){.nl_hdr.nlmsg_len = message_len,
+                           .nl_hdr.nlmsg_type = RTM_NEWLINK,
+                           .nl_hdr.nlmsg_flags = NLM_F_REQUEST,
+                           .nl_hdr.nlmsg_seq = (uint32_t)time(NULL),
+                           .info_hdr.ifi_family = AF_UNSPEC,
+                           .info_hdr.ifi_index = if_index},
+         sizeof(struct ndm_hdr));
+  memcpy(buff_iov + sizeof(struct ndm_hdr), buff_nla, nla_len); // TODO size
+  struct iovec iov = {.iov_base = buff_iov, .iov_len = message_len};
+  struct msghdr mh = {
+      .msg_name = &sa,
+      .msg_namelen = sizeof(sa),
+      .msg_iov = &iov,
+      .msg_iovlen = 1,
+  };
+  sendmsg(fd, &mh, 0);
+
+  close(fd);
+
   return 0;
 }
 
@@ -190,7 +195,7 @@ int main(int argc, char **argv) {
     if (!strcmp(argv[1], "addif"))
       return bridge_add_iface(argv[2], argv[3]);
     if (!strcmp(argv[1], "delif"))
-      return bridge_del_iface(argv[2], argv[3]);
+      return bridge_del_iface(argv[3]);
   }
   return help(argv[0]);
 }
